9cdb727720fd2407b3fed7e279755807ebc65244
SuseongJr <robert19650404@gmail.com>
2024-01-03 23:28:11 +0900

    Can't make table!!!!

---
+++ security1/src/main/java/com/cos/security1/model/User.java
@@ -0,0 +1,35 @@
+package com.cos.security1.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+
+import javax.persistence.GenerationType;
+
+@Entity
+@Data
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String username;
+	
+	private String password;
+	
+	private String email;
+	
+	private String role;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+
+}
